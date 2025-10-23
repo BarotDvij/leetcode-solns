f7209cc2c66f7d3763433a5d4c7958edac8d4427
Dvij Barot <122646938+BarotDvij@users.noreply.github.com>
2025-10-23 00:30:41 -0400

    File updated at 10/23/2025, 12:30:41 AM

--- 2390-RemovingStarsFromaString/2390-RemovingStarsFromaString.py
+++ 2390-RemovingStarsFromaString/2390-RemovingStarsFromaString.py
@@ -1,11 +1,10 @@
-# Last updated: 10/23/2025, 12:09:54 AM
+# Last updated: 10/23/2025, 12:30:41 AM
 class Solution:
     def removeStars(self, s: str) -> str:
         starless = []
         for i in range(len(s)):
-            starless += s[i]
+            starless.append(s[i])
             if s[i] == '*':
                 starless.pop()
                 starless.pop()
         return ''.join(starless)
-        
